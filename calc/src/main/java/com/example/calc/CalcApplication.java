e99365593478b4548825814ea0e5c569c68d3f8b
Dhanniya preethi <94435126+Dhanniyapreethi@users.noreply.github.com>
2022-02-14 17:55:01 +0530

    Add files via upload

---
+++ calc/src/main/java/com/example/calc/CalcApplication.java
@@ -0,0 +1,15 @@
+package com.example.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class CalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcApplication.class, args);
+    }
+}
+
---
+++ calc/src/main/java/com/example/calc/beans/Numbers.java
@@ -0,0 +1,31 @@
+package com.example.calc.beans;
+
+public class Numbers {
+    private int a=10;
+    private int b=8;
+    private int c;
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
